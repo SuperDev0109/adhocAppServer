96c1c7da57f478832eb9f7a3fb1032a8a93cfc6b
SuperDev0109 <nguyenjame440@gmail.com>
2022-06-30 03:32:34 +0800

    added

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ module.exports = function override(config, env) {
     assert: require.resolve("assert"),
     crypto: require.resolve("crypto-browserify"),
     http: require.resolve("stream-http"),
-    https: require.resolve("https-browserify"),
+    // https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify/browser"),
     buffer: require.resolve("buffer"),
     stream: require.resolve("stream-browserify"),
